8022b64ab60a370509f7f2fb63f5053552582966
Alteon Alves Cruz <alteonalves@gmail.com>
2024-02-26 20:49:29 -0300

    feat(controller): Add controller layer

---
+++ src/main/kotlin/me/alteon/creditapplicationsystem/controller/CustomerResource.kt
@@ -0,0 +1,20 @@
+package me.alteon.creditapplicationsystem.controller
+
+import me.alteon.creditapplicationsystem.dto.CustomerDto
+import me.alteon.creditapplicationsystem.service.impl.CustomerService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/customers")
+class CustomerResource(private val customerService: CustomerService) {
+
+    @PostMapping
+    fun saveCustomer(@RequestBody customerDto: CustomerDto): String {
+        val savedCustomer = this.customerService.save(customerDto.toEntity())
+        return "Customer ${savedCustomer.email} saved!"
+    }
+
+}
---
+++ src/main/kotlin/me/alteon/creditapplicationsystem/dto/CustomerDto.kt
@@ -0,0 +1,26 @@
+package me.alteon.creditapplicationsystem.dto
+
+import me.alteon.creditapplicationsystem.entity.Address
+import me.alteon.creditapplicationsystem.entity.Customer
+import java.math.BigDecimal
+
+data class CustomerDto(
+    val firstName: String,
+    val lastName: String,
+    val cpf: String,
+    val income: BigDecimal,
+    val email: String,
+    val password: String,
+    val zipCode: String,
+    val street: String,
+) {
+    fun toEntity(): Customer = Customer(
+        firstName = this.firstName,
+        lastName = this.lastName,
+        cpf = this.cpf,
+        email = this.email,
+        income = this.income,
+        password = this.password,
+        address = Address(zipCode = this.zipCode, street = this.street)
+    )
+}
