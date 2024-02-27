36fa9faa2f2c750037afda7788aac2397ad32a6e
Alteon Alves Cruz <alteonalves@gmail.com>
2024-02-26 23:42:11 -0300

    feat(controller):Add controller layers

--- src/main/kotlin/me/alteon/creditapplicationsystem/controller/CustomerResource.kt
+++ src/main/kotlin/me/alteon/creditapplicationsystem/controller/CustomerResource.kt
@@ -1,11 +1,11 @@
 package me.alteon.creditapplicationsystem.controller
 
 import me.alteon.creditapplicationsystem.dto.CustomerDto
+import me.alteon.creditapplicationsystem.dto.CustomerUpdateDto
+import me.alteon.creditapplicationsystem.dto.CustomerView
+import me.alteon.creditapplicationsystem.entity.Customer
 import me.alteon.creditapplicationsystem.service.impl.CustomerService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/customers")
@@ -15,6 +15,26 @@ class CustomerResource(private val customerService: CustomerService) {
     fun saveCustomer(@RequestBody customerDto: CustomerDto): String {
         val savedCustomer = this.customerService.save(customerDto.toEntity())
         return "Customer ${savedCustomer.email} saved!"
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): CustomerView {
+        val customer: Customer = this.customerService.findById(id)
+        return CustomerView(customer)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteCustomer(@PathVariable id: Long) = this.customerService.delete(id)
+
+    @PatchMapping
+    fun updateCustomer(
+        @RequestParam(value = "customerId") id: Long,
+        @RequestBody customerUpdateDto: CustomerUpdateDto
+    ): CustomerView {
+        val customer: Customer = this.customerService.findById(id)
+        val customerToUpdate: Customer = customerUpdateDto.toEntity(customer)
+        val customerUpdated: Customer = this.customerService.save(customerToUpdate)
+        return CustomerView(customerUpdated)
     }
 
 }
